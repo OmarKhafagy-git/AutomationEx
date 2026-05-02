7489fe71338dcda309a4e59c7d34c97223297e1c
Omar Khafagy <omar.khafagy2222@gmail.com>
2026-05-02 04:32:06 +0300

    Initial framework commit

--- src/main/java/Pages/ProductsDetails.java
+++ src/main/java/Pages/ProductsDetails.java
@@ -13,6 +13,33 @@ public class ProductsDetails {
     By ProductDetails = By.xpath("/html/body/section/div/div/div[2]/div[2]/div[2]/div");
     By IncreaseQuan = By.xpath("//*[@id=\"quantity\"]");
     By AddToCart = By.xpath("/html/body/section/div/div/div[2]/div[2]/div[2]/div/span/button");
+    By WriteReviewTxd = By.xpath("/html/body/section/div/div/div[2]/div[3]/div[1]/ul/li/a");
+    By Name = By.xpath("//*[@id=\"name\"]");
+    By Email = By.xpath("//*[@id=\"email\"]");
+    By AddReview = By.xpath("//*[@id=\"review\"]");
+    By Submit = By.xpath("//*[@id=\"button-review\"]");
+    By successMsg = By.xpath("//*[@id=\"review-section\"]/div/div/span");
+
+
+
+
+
+    public String SuccessMsg() {
+        return driver.findElement(successMsg).getText();
+
+    }
+
+
+    public boolean isWriteReviewVisible() {
+        return driver.findElement(WriteReviewTxd).isDisplayed();
+    }
+
+    public void addReview(String name, String email, String review) {
+        driver.findElement(Name).sendKeys(name);
+        driver.findElement(Email).sendKeys(email);
+        driver.findElement(AddReview).sendKeys(review);
+        driver.findElement(Submit).click();
+    }
 
 
 
